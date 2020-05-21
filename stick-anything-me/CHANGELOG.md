ba0fa4e3569261e4f130bb42b71d4dbf01729d23
themissile1234 <43247168+themissile1234@users.noreply.github.com>
2020-05-21 13:59:44 +0300

    Add files via upload

---
+++ stick-anything-me/CHANGELOG.md
@@ -0,0 +1,60 @@
+<a name="1.3.0"></a>
+# 1.3.0 (2020-01-25)
+
+
+
+<a name="1.2.1"></a>
+## 1.2.1 (2017-04-11)
+
+
+### Features
+
+* **Build:** External markup support ([382b3c4](https://github.com/martinmethod/stickme/commit/382b3c4))
+
+
+
+<a name="1.2.0"></a>
+# 1.2.0 (2017-04-05)
+
+
+### Bug Fixes
+
+* **Google Analytics:** Missed ID ([f188d92](https://github.com/martinmethod/stickme/commit/f188d92))
+
+
+### Features
+
+* **Deploy:** npm, GitHub Pages & GitHub Releases ([09a63ef](https://github.com/martinmethod/stickme/commit/09a63ef))
+
+
+
+<a name="1.1.3"></a>
+## 1.1.3 (2017-04-02)
+
+
+### Bug Fixes
+
+* **npm deployment:** Changed token ([72c61f9](https://github.com/martinmethod/stickme/commit/72c61f9))
+
+
+
+<a name="1.1.1"></a>
+## 1.1.1 (2017-04-02)
+
+
+### Bug Fixes
+
+* **Docs:** Broken links ([2c7b1c0](https://github.com/martinmethod/stickme/commit/2c7b1c0))
+
+
+
+<a name="1.1.0"></a>
+# 1.1.0 (2017-04-02)
+
+
+### Features
+
+* **Building system:** Refactored building system ([ec53b27](https://github.com/martinmethod/stickme/commit/ec53b27))
+
+
+
---
+++ stick-anything-me/Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+  var path = require('path'),
+      assets = grunt.file.exists('../assets.json') ?
+               grunt.file.readJSON('../assets.json').path :
+               'node_modules/my-jquery-plugins-assets/',
+      common = grunt.file.readJSON(assets + 'configs/common.json'),
+      pkg = grunt.file.readJSON('package.json'),
+      settings = grunt.file.readJSON('configs/settings.json'),
+      banner = require(assets + 'configs/banner'),
+      env = grunt.option('target') || 'dev',
+      prod = env === 'prod';
+
+  grunt.pluginData = {
+    assets: assets,
+    pkg: pkg,
+    common: common,
+    settings: settings,
+    prod: prod,
+    env: env,
+
+    envPath: !prod ?
+             'assets/page/' :
+             'https://assets.' + common.domain + '/',
+
+    banner: banner,
+
+    domain: pkg.name.toLowerCase() + '.' + common.domain
+  };
+
+  require('time-grunt')(grunt);
+
+  require('load-grunt-config')(grunt, {
+    configPath: path.join(process.cwd(), 'configs/grunt'),
+    data: grunt.pluginData
+  });
+
+  grunt.registerTask('build', function() {
+    grunt.task.run(['concurrent:build']);
+    grunt.task.run(['concurrent:' + env]);
+  });
+};
